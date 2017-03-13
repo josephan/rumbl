bbb69237850aa45433352b58feb60a82ec331bf2
Joseph An <an.joseph891@gmail.com>
2017-03-12 22:58:13 -0400

    Testing
    * setup basic shared testing functions
    * write integration tests
    * test authentication plugin in isolation
    * test views
    * test models with and without side effects

---
+++ test/models/user_test.exs
@@ -0,0 +1,27 @@
+defmodule Rumbl.UserTest do
+  use Rumbl.ModelCase, async: true
+  alias Rumbl.User
+
+  @valid_attrs %{name: "A User", username: "eva", password: "secret"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "registration_changeset with valid attributes hashes password" do
+    attrs = Map.put(@valid_attrs, :password, "123456")
+    changeset = User.registration_changeset(%User{}, attrs)
+    %{password: pass, password_hash: pass_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert pass_hash
+    assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
+  end
+end
